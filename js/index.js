5351a4f3c5966af0f84e02ad51140fa15c6e7a52
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-06-28 23:35:47 -0300

    using firebase

--- js/index.js
+++ js/index.js
@@ -1,105 +1,19 @@
-const teams = [
-    {
-        id:"1",
-        name:"América",
-        imageLink:"assets/america.png"
-    },
-    {
-        id:"2",
-        name:"Athlético",
-        imageLink:"assets/athletico.png"
-    },
-    {
-        id:"3",
-        name:"Atlético Go",
-        imageLink:"assets/atleticogo.png"
-    },
-    {
-        id:"4",
-        name:"Atletico mg",
-        imageLink:"assets/atleticomg.png"
-    },
-    {
-        id:"5",
-        name:"Bahia",
-        imageLink:"assets/bahia.png"
-    },
-    {
-        id:"6",
-        name:"Bragantino",
-        imageLink:"assets/bragantino.png"
-    },
-    {
-        id:"7",
-        name:"Ceará",
-        imageLink:"assets/ceara.png"
-    },
-    {
-        id:"8",
-        name:"Chapecoense",
-        imageLink:"assets/chapecoense.png"
-    },
-    {
-        id:"9",
-        name:"Corinthians",
-        imageLink:"assets/corinthians.png"
-    },
-    {
-        id:"10",
-        name:"Cuiabá",
-        imageLink:"assets/cuiaba.png"
-    },
-    {
-        id:"11",
-        name:"Flamengo",
-        imageLink:"assets/flamengo.png"
-    },
-    {
-        id:"12",
-        name:"Fluminense",
-        imageLink:"assets/fluminense.png"
-    },
-    {
-        id:"13",
-        name:"Fortaleza",
-        imageLink:"assets/fortaleza.png"
-    },
-    {
-        id:"14",
-        name:"Grêmio",
-        imageLink:"assets/gremio.png"
-    },
-    {
-        id:"15",
-        name:"Internacional",
-        imageLink:"assets/internacional.png"
-    },
-    {
-        id:"16",
-        name:"Juventude",
-        imageLink:"assets/juventude.png"
-    },
-    {
-        id:"17",
-        name:"Palmeiras",
-        imageLink:"assets/palmeiras.png"
-    },
-    {
-        id:"18",
-        name:"Santos",
-        imageLink:"assets/santos.png"
-    },
-    {
-        id:"19",
-        name:"São paulo",
-        imageLink:"assets/saopaulo.png"
-    },
-    {
-        id:"20",
-        name:"Sport",
-        imageLink:"assets/sport.png"
-    }
-];
+
+var firebaseConfig = {
+apiKey: "AIzaSyCeovbXd6n0XscLGddEFoeBdiYDGdsPoQ4",
+authDomain: "teams-8d01d.firebaseapp.com",
+projectId: "teams-8d01d",
+storageBucket: "teams-8d01d.appspot.com",
+messagingSenderId: "945903313491",
+appId: "1:945903313491:web:7e5c3b8a1e8e319d181ca6",
+measurementId: "G-C88GRXHFT3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+let database = firebase.firestore();
+
 
 function createElement(elementType, elementClass, parent, imageParams){
     const element = document.createElement(elementType); 
@@ -114,18 +28,21 @@ function createElement(elementType, elementClass, parent, imageParams){
     return(element);
 }
 
-function renderTeamList(teamName, teamImageLink){
+function renderTeamList(teamName, teamImageLink, state){
     const brandsList = document.querySelector('ul.brands__list');
     const brandItem = createElement("li", "brand__item", brandsList,null);
     const anchor = createElement("a", null, brandItem);
     const brandItemImage = createElement ("div", "brand__item__image", anchor,null);
     const image = createElement("img", null, brandItemImage,{alt:teamName, src:teamImageLink});
     const brandItemTitle = createElement("h4",'brand__item__title', anchor,null);
-    brandItemTitle.innerText=teamName;
+    brandItemTitle.innerText=teamName + ` (${state})`;
 } 
 document.addEventListener('DOMContentLoaded', ()=>{
-       teams.forEach(team => {
-           renderTeamList(team.name,team.imageLink);
-       });
+       database.collection('teams').get().then((snapshot)=>{
+        snapshot.forEach((document)=>{
+            let team = document.data();
+            renderTeamList(team.name,`assets/${team.image}`,team.state);
+        })
+    })
 
 })
